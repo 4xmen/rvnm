3e7182626e20ecb7e71f256e9492bc565add7149
A1Gard <a1gard@4xmen.ir>
2017-12-08 02:07:55 +0330

    initial required files

---
+++ rvnm/public_html/dist/jquery.rvnm.js
@@ -0,0 +1,17 @@
+;(function ($) {
+
+    $.fn.rvnm = function (options) {
+
+        var settings = $.extend({
+            wrapper:'#wrapper'
+        }, options);
+
+
+        this.each(function () {
+
+        });
+
+
+    };
+
+}(jQuery));
